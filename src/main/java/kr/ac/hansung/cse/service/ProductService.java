343b5f2e279fd40584d7021886e599669a3ffc0c
uz030 <sseo08267@hansung.ac.kr>
2026-04-18 01:55:02 +0900

    feat: 상품 검색 화면 수정 및 기능 구현

--- src/main/java/kr/ac/hansung/cse/service/ProductService.java
+++ src/main/java/kr/ac/hansung/cse/service/ProductService.java
@@ -133,4 +133,10 @@ public class ProductService {
     public void deleteProduct(Long id) {
         productRepository.delete(id);
     }
+
+    public List<Product> searchByName(String keyword) {
+        return productRepository.findByNameContaining(keyword); }
+    public List<Product> searchByCategory(Long categoryId) {
+        return productRepository.findByCategoryId(categoryId); }
+
 }
